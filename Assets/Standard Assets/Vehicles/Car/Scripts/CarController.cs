b3646d500e8c8bf33b1b16035615a7a65f121ece
Chiara PERRIN <90776753+Chiaraperrin@users.noreply.github.com>
2022-06-01 18:25:26 +0200

    Mise en place Mirror

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
-using Photon.Pun;
+using Mirror;
 
 #pragma warning disable 649
 namespace UnityStandardAssets.Vehicles.Car
@@ -19,7 +19,7 @@ namespace UnityStandardAssets.Vehicles.Car
         KPH
     }
 
-    public class CarController : MonoBehaviour
+    public class CarController : NetworkBehaviour
     {
         public Text TxtSpeed;
         [SerializeField] private CarDriveType m_CarDriveType = CarDriveType.FourWheelDrive;
@@ -60,7 +60,6 @@ namespace UnityStandardAssets.Vehicles.Car
         public float AccelInput { get; private set; }
         public float Speed;
 
-        PhotonView view;
 
         // Use this for initialization
         private void Start()
@@ -76,7 +75,6 @@ namespace UnityStandardAssets.Vehicles.Car
 
             m_Rigidbody = GetComponent<Rigidbody>();
             m_CurrentTorque = m_FullTorqueOverAllWheels - (m_TractionControl*m_FullTorqueOverAllWheels);
-            view = GetComponent<PhotonView>();
         }
 
 
@@ -142,47 +140,51 @@ namespace UnityStandardAssets.Vehicles.Car
 
         public void Move(float steering, float accel, float footbrake, float handbrake)
         {
-            for (int i = 0; i < 4; i++)
+            if (this.isLocalPlayer)
             {
-                Quaternion quat;
-                Vector3 position;
-                m_WheelColliders[i].GetWorldPose(out position, out quat);
-                m_WheelMeshes[i].transform.position = position;
-                m_WheelMeshes[i].transform.rotation = quat;
+                for (int i = 0; i < 4; i++)
+                {
+                    Quaternion quat;
+                    Vector3 position;
+                    m_WheelColliders[i].GetWorldPose(out position, out quat);
+                    m_WheelMeshes[i].transform.position = position;
+                    m_WheelMeshes[i].transform.rotation = quat;
+                }
+
+                //clamp input values
+                steering = Mathf.Clamp(steering, -1, 1);
+                AccelInput = accel = Mathf.Clamp(accel, 0, 1);
+                BrakeInput = footbrake = -1 * Mathf.Clamp(footbrake, -1, 0);
+                handbrake = Mathf.Clamp(handbrake, 0, 1);
+
+                //Set the steer on the front wheels.
+                //Assuming that wheels 0 and 1 are the front wheels.
+                m_SteerAngle = steering * m_MaximumSteerAngle;
+                m_WheelColliders[0].steerAngle = m_SteerAngle;
+                m_WheelColliders[1].steerAngle = m_SteerAngle;
+
+                SteerHelper();
+                ApplyDrive(accel, footbrake);
+                CapSpeed();
+
+                //Set the handbrake.
+                //Assuming that wheels 2 and 3 are the rear wheels.
+                if (handbrake > 0f)
+                {
+                    var hbTorque = handbrake * m_MaxHandbrakeTorque;
+                    m_WheelColliders[2].brakeTorque = hbTorque;
+                    m_WheelColliders[3].brakeTorque = hbTorque;
+                }
+
+
+                CalculateRevs();
+                GearChanging();
+
+                AddDownForce();
+                CheckForWheelSpin();
+                TractionControl();
             }
-
-            //clamp input values
-            steering = Mathf.Clamp(steering, -1, 1);
-            AccelInput = accel = Mathf.Clamp(accel, 0, 1);
-            BrakeInput = footbrake = -1*Mathf.Clamp(footbrake, -1, 0);
-            handbrake = Mathf.Clamp(handbrake, 0, 1);
-
-            //Set the steer on the front wheels.
-            //Assuming that wheels 0 and 1 are the front wheels.
-            m_SteerAngle = steering*m_MaximumSteerAngle;
-            m_WheelColliders[0].steerAngle = m_SteerAngle;
-            m_WheelColliders[1].steerAngle = m_SteerAngle;
-
-            SteerHelper();
-            ApplyDrive(accel, footbrake);
-            CapSpeed();
-
-            //Set the handbrake.
-            //Assuming that wheels 2 and 3 are the rear wheels.
-            if (handbrake > 0f)
-            {
-                var hbTorque = handbrake*m_MaxHandbrakeTorque;
-                m_WheelColliders[2].brakeTorque = hbTorque;
-                m_WheelColliders[3].brakeTorque = hbTorque;
-            }
-
-
-            CalculateRevs();
-            GearChanging();
-
-            AddDownForce();
-            CheckForWheelSpin();
-            TractionControl();
+           
         }
 
 
