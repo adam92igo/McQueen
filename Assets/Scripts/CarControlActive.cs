52b04b7709e391df369b4aaaa414efd2c26485c5
adam92igo <adam.bokobza@epita.fr>
2022-06-02 23:20:42 +0200

    Ajout pour Mirror

--- Assets/Scripts/CarControlActive.cs
+++ Assets/Scripts/CarControlActive.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Vehicles.Car;
+using Mirror;
 
 public class CarControlActive : MonoBehaviour
 {
