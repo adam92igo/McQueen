9aa0cc33474527bc4eedfe7b510056bcdbee8487
Chiara PERRIN <90776753+Chiaraperrin@users.noreply.github.com>
2022-06-03 15:56:38 +0200

    rien

--- Assets/Scripts/CarControlActive.cs
+++ Assets/Scripts/CarControlActive.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Vehicles.Car;
-using Mirror;
+
 
 public class CarControlActive : MonoBehaviour
 {
@@ -11,11 +11,13 @@ public class CarControlActive : MonoBehaviour
     public GameObject Dreamcar01;
     void Start()
     {
-        
-            CarControl.GetComponent<CarController>().enabled = true;
-            CarControl.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-            Dreamcar01.GetComponent<CarAIControl>().enabled = true;
-       
+        CarControl.GetComponent<Countdown>().enabled = true;
+        CarControl.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+
+
+        Dreamcar01.GetComponent<CarAIControl>().enabled = true;
+
+
 
     }
 }
