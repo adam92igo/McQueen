0462cf24bdbafbb2e4d991a250feda33217b46d5
ibeaudrey <102725384+ibeaudrey@users.noreply.github.com>
2022-06-06 17:40:25 +0200

    ajout menu pause

--- Assets/Pausemenu.cs
+++ Assets/Pausemenu.cs
@@ -6,6 +6,7 @@ public class Pausemenu : MonoBehaviour
 {
     public static bool gameIsPaused = false;
     public GameObject pauseMenuUI;
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -16,18 +17,11 @@ public class Pausemenu : MonoBehaviour
             }
             else
             {
-                Paused();
+                pauseMenuUI.SetActive(true);
+                Time.timeScale = 0;
+                gameIsPaused = true;
             }
-
-            Debug.Log("Hello PauseMenu");
         }
-    }
-
-    public void Paused()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0;
-        gameIsPaused = true;
     }
 
     public void Resume()
